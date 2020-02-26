41f4183593e2cc85812c38ebbf0b4fb0ae2e0bf9
killyoudarlings <jlee286@artic.edu>
2020-02-25 19:43:40 -0600

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 # Establish a session.
@@ -17,27 +18,56 @@ response = session.get(url)
 soup = BeautifulSoup(response.content, 'lxml')
 
 # Extract the items. The class name was determined by inspecting the HTML.
-items = soup.findAll('div', { 'class': 'sh-dlr__content' })
+items = soup.findAll('div', {'class': 'sh-dlr__content'})
 
 results = []
 
-# Iterate through the items.
-for item in items:
-    # All selectors were determined by inspection and will probably change.
-    full_description_html = item.find('div', {'class': 'hBUZL'})
-    title = item.find('h3').text
-    price = item.find('span', {'aria-hidden': 'true'}).text
-    merchant = full_description_html.find('a').text
-    brief_description = item.findAll('div', {'class':'hBUZL'})[-1].text
-    
-    results.append(
-        {
-           'title': title,
-           'price': price,
-           'merchant': merchant,
-           'brief_description': brief_description 
-        }
-    )
+with open('employee_file.csv', mode='w') as employee_file:
+    employee_writer = csv.writer(
+        employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    employee_writer.writerow(["TITLE", "PRICE", "MERCHANT", "DESCRIPTION"])
+    # Iterate through the items.
+    for item in items:
+        # All selectors were determined by inspection and will probably change.
+        try:
+            full_description_html = item.find('div', {'class': 'hBUZL'})
+        except:
+            full_description_html = "NONE"
 
+        try:
+            title = item.find('h3').text
+        except:
+            title = "NONE"
+            print("No Title")
 
-    
+        try:
+            price = item.find('span', {'aria-hidden': 'true'}).text
+        except:
+            price = "NONE"
+            print("No Price")
+
+        try:
+            merchant = full_description_html.find('a').text
+        except:
+            merchant = "NONE"
+            print("No Merchant")
+
+        try:
+            brief_description = item.findAll('div', {'class': 'hBUZL'})[-1].text
+        except:
+            brief_description="NONE"
+            print("No Brief Description")
+
+        employee_writer.writerow([title, price, merchant, brief_description])
+
+        results.append(
+            {
+                'title': title,
+                'price': price,
+                'merchant': merchant,
+                'brief_description': brief_description
+            }
+        )
+        
+
+print("Ok!")
